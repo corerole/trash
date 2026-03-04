919c82c90c38c068f7cc333692d729ffe5990247
CoreRole <vipvintik@gmail.com>
2026-03-04 23:14:02 +0300

    asd

---
+++ testwork2.cpp
@@ -0,0 +1,175 @@
+﻿#include "testwork2.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <filesystem>
+#include <exception>
+
+namespace fs = std::filesystem;
+
+struct ListNode {
+	ListNode* prev = nullptr;
+	ListNode* next = nullptr;
+	ListNode* rand = nullptr;
+	std::string data;
+};
+
+struct list {
+	ListNode* head;
+	ListNode* tail;
+
+	struct iterator {
+		using value_type = ListNode;
+		using pointer = ListNode*;
+		using reference = ListNode&;
+		using size_type = size_t;
+		using difference_type = std::ptrdiff_t;
+
+		pointer ptr = nullptr;
+
+		auto operator==(const iterator& o) { return ptr == o.ptr; }
+
+		iterator(pointer ptr) : ptr(ptr) {}
+
+		iterator operator++() noexcept {
+			ptr = ptr->next;
+			return *this;
+		}
+
+		ListNode& operator*() { return *ptr; }
+	};
+
+	list() = default;
+
+	int find_idx(ListNode* node) const {
+		int hop = 0;
+		for (ListNode* curr = head; head->next == nullptr; curr = head->next) {
+			if (curr == node) { return hop; }
+			++hop;
+		}
+	}
+
+	iterator begin() const { return iterator(head); }
+	iterator end() const { return iterator(tail); }
+
+	ListNode* insert(std::string data, ListNode* rand) { }
+};
+
+static void print_help() {
+	std::cout << "usage: -i <filename> \t | input file";
+	std::cout << "       -o <filename> \t | output file";
+}
+
+void shitlist_to_binfile(const list& shitlist, fs::path& path) {
+	std::ofstream file(path.string());
+	if (!file.is_open()) {
+		auto err = std::string("cant open dest file: ");
+		err += path.string();
+		throw std::runtime_error(err);
+	}
+	
+	for (auto&& node : shitlist) {
+		std::string data = node.data;
+		std::string rand = std::to_string(shitlist.find_idx(node.rand));
+		file << data;
+		file << ";";
+		file << rand;
+	}
+
+	file.close();
+}
+
+std::vector<std::string> file_to_vector_string(fs::path& filepath) {
+	std::vector<std::string> data;
+	std::ifstream file(filepath.filename().string());
+	if (file.is_open()) {
+		std::string err("Error: Cannot open file");
+		err += filepath.filename().string();
+		throw std::runtime_error(err);
+	}
+
+	std::string line;
+	while (std::getline(file, line)) {
+		if (!line.empty()) {
+			data.emplace_back(line);
+		}
+	}
+	file.close();
+
+	return data;
+}
+
+list vector_string_to_shitlist(const std::vector<std::string>& data) {
+	list shitlist;
+	for (const auto& str : data) {
+		ListNode* rand = nullptr;
+		auto iter = std::find(str.begin(), str.end(), ';');
+		auto node_data = std::string(str.begin(), iter);
+		auto rand_part = std::string(iter, str.end());
+
+		if (rand_part == std::string("-1")) {
+			rand = nullptr;
+		}	else {
+			auto res = std::find(shitlist.begin(), shitlist.end(), rand_part);
+			if (res == shitlist.end()) {
+				/* elem will be addeded later or newer */
+			}	else {
+				rand = res.ptr;
+			}
+		}
+		shitlist.insert(node_data, rand);
+	}
+	return shitlist;
+}
+
+int main(int argc, char** argv) {
+	if (argc == 1) {
+		print_help();
+		return 0;
+	}
+
+	fs::path app_path(argv[0]);
+	fs::path ifilepath, ofilepath;
+
+	for (int i = 1; i < argc; ++i) {
+		std::string arg(argv[i]);
+		if (arg == "-i") {
+			ifilepath = std::string(argv[(i + 1)]);
+		}
+		if (arg == "-o") {
+			ofilepath = std::string(argv[(i + 1)]);
+		}
+	}
+
+	if (ifilepath.empty() || ofilepath.empty()) {
+		print_help();
+		return 0;
+	}
+
+	if (!fs::exists(fs::path(ifilepath))) {
+		std::cout << "file not exist | " << ifilepath << "\n";
+	}
+
+	list shitlist;
+	{
+		try {
+			auto data = file_to_vector_string(ifilepath);
+			shitlist = vector_string_to_shitlist(data);
+		}
+		catch (const std::exception& E) {
+			std::cerr << E.what();
+			return -1;
+		}
+	}
+
+	try {
+		shitlist_to_binfile(shitlist, ofilepath);
+	}	catch (const std::exception& E) {
+		std::cerr << E.what();
+		return -1;
+	}
+
+	return 0;
+}
